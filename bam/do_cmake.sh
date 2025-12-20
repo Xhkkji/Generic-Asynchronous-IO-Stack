f4b2fe94790144c61b46e35d22b360a319658c7a
Xhkkji <1292974688@qq.com>
2025-12-21 02:58:23 +0800

    将所有异步函数初步定义完毕，read_data_async已经定义好

--- bam/do_cmake.sh
+++
@@ -1,7 +0,0 @@
-git submodule update --init --recursive
-mkdir -p build; cd build
-cmake ..
-make libnvm                         # builds library
-make benchmarks                     # builds benchmark program
-cd build/module
-make
--- gids_module/do_cmake.sh
+++ gids_module/do_cmake.sh
@@ -1,3 +1,4 @@
+# 所有对bam系统代码的修改在此处重新编译即可
 cd build
 cmake .. && make -j
 cd BAM_Feature_Store
