e8852628672437289775287f9f0ee335c91c8ed7
Xhkkji <1292974688@qq.com>
2026-05-14 21:44:37 +0800

    整理了evaluation和cnn_evaluation文件夹

--- evaluation/cids_prepare_dataset_imagenet1k.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-PYTHON_BIN=/home/xhk/miniconda3/envs/pytorch/bin/python
-PREP_SCRIPT=/home/xhk/hyperion/GIDS/evaluation/cids_prepare_dataset_imagenet1k.py
-
-"${PYTHON_BIN}" "${PREP_SCRIPT}" "$@"
